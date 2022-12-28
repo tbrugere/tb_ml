021325291caea6dbf668c5078b62cc05a253a7e6
Tristan BRUGÈRE <67050900+nephanth@users.noreply.github.com>
2022-12-28 16:01:28 +0100

    datasets

---
+++ tb_ml/datasets/base_classes.py
@@ -0,0 +1,46 @@
+from typing import TypeVar, Generic, Iterator
+
+from dataclasses import dataclass
+# from typing import Callable, TypeAlias
+
+import torch.utils.data as torch_data
+
+Element = TypeVar("Element")
+Element2 = TypeVar("Element2")
+
+class Dataset(torch_data.Dataset, Generic[Element]):
+    def __getitem__(self, i) -> Element:
+        raise NotImplementedError
+
+    def __iter__(self) -> Iterator[Element]:
+        raise NotImplementedError
+
+
+@dataclass
+class Transform(Dataset[Element2], Generic[Element, Element2]):
+    """
+    Transformation of datasets.
+
+    Contrarily to the ones seen in pytorch 
+
+    Basically they are actually datasets, which take other datasets as parameter.
+
+    This base class should not be instantiated, it doesn’t do anything.
+
+    Since this is a dataclass, the __init__() will call __post_init__(), so subclasses could
+    implement that
+    """
+    inner: Dataset[Element]
+
+
+    def __post_init__(self):
+        self._initialize()
+
+    def _initialize(self):
+        pass
+
+    def __len__(self):
+        return len(self.inner)#type: ignore
+
+# class IterableTransform(Transform, IterableDataset):
+#     pass
---
+++ tb_ml/datasets/transforms.py
@@ -0,0 +1,107 @@
+"""
+Transformation of datasets.
+
+Contrarily to the ones seen in pytorch 
+
+Basically they are actually datasets, which take other datasets as parameter.
+
+"""
+from collections import namedtuple
+from collections.abc import Sequence, Mapping
+from dataclasses import dataclass, field
+import torch
+
+from .base_classes import Transform, Dataset, Element, Element2
+from .registration import transform_register
+
+@transform_register
+@dataclass
+class RenameTransform(Transform[Element, Element2]):
+    """Rename the outputs of a dataset
+
+    Converts a dataset whose elements are tuples, dicts, or namedtuples to
+    dataset whose elements are namedtuples, 
+    using a name correspondance `named_map`.
+
+    More precisely, 
+
+    If 
+    ```python
+    named_map = {
+        input_name1: "output_name1", 
+        input_name2: "output_name2",
+        input_name3: "output_name3"
+    }
+    ```
+
+    and `dataset` is the input dataset, whose elements are namedtuples, 
+    dicts, or tuples
+
+    Then `t = RenameTransform(dataset, name_map)[i]` is a namedtuple, 
+    with fields "output_name1", "output_name2" and "output_name3"
+
+    whose values are resp. `dataset[i][input_name1], dataset[i][input_name2], dataset[i][input_name3]`
+    (or the fields if `dataset[i]` is a `namedtuple`)
+
+    The input names can be either 
+    - The special name "_", which is mapped to the entire input `dataset[i]`
+    - A string if dataset’s elements are mappings 
+        (in which case __getitem__ is used)
+    - An int if dataset’s elements are sequences
+        (in which case __getitem__ is used)
+    - A string if datasets’s elements are objects 
+        (in which case __getattr__ is used)
+
+    """
+    inner: Dataset[Element]
+    name_map: dict[int | str, str]
+    datapoint_type: type[Element2] = field(init=False)
+
+    def __post_init__(self):
+        self.element_type= namedtuple("datapoint", self.name_map.values())
+
+    def __getitem__(self, index) -> Element2:
+        inner_value: Element = self.inner[index]
+        d = {}
+        for key, field_name in self.name_map.items():
+            match key:
+                case "_":
+                    d[field_name] = inner_value
+                case int() if isinstance(inner_value, Sequence):
+                    d[field_name] = inner_value[key]
+                case str() if isinstance(inner_value, Mapping):
+                    d[field_name] = inner_value[key]
+                case str():
+                    d[field_name] = getattr(inner_value, key)
+                case _:
+                    assert False, f"incompatible {key=} and {inner_value=}"
+
+        return self.datapoint_type(**d)
+
+
+############################################################################
+# Normalization
+############################################################################
+
+
+@transform_register
+class TensorUniqueTransform(Transform[torch.Tensor, torch.Tensor]):
+    """Deletes unique elements in a dataset
+    assumes all elements in the dataset are torch tensors
+    and that the dataset is sliceable (ie you can get the full tensor by using Dataset[:])
+    """
+    inner: Dataset[torch.Tensor]
+
+    data: torch.Tensor
+
+    def _initialize(self):
+        self.data = torch.unique(self.inner[:], dim=0)
+
+    def __getitem__(self, *args):
+        return self.data.__getitem__(*args)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        return iter(self.data)
