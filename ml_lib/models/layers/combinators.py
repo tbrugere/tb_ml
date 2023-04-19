41bb8d166319c9e5b051505f150da182bd9e76d9
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2023-04-19 13:35:21 -0700

    added split combinator and custom version of Sequential with nicer parameter handling

--- ml_lib/models/layers/combinators.py
+++ ml_lib/models/layers/combinators.py
@@ -1,5 +1,6 @@
 import functools as ft
 import itertools as it # pyright: ignore
+from inspect import signature
 
 import torch
 import torch.nn as nn
@@ -36,4 +37,44 @@ class Repeat(nn.Sequential):
     def __init__(self, n, module_factory):
         super().__init__(*[module_factory() for _ in range(n)])
 
+class Split(nn.Module):
+    """
+    A module that splits the input into n parts and returns them as a tuple.
+    """
 
+    def __init__(self, n=2):
+        super().__init__()
+        self.n = n
+
+    def forward(self, x):
+        return tuple(x.chunk(self.n, dim=-1))
+    
+
+class Sequential(nn.Module):
+    """Sequential implementation that allows for more than one input and output."""
+    sub_modules: nn.ModuleList
+    def __init__(self, *modules: nn.Module):
+        super().__init__()
+        self.sub_modules = nn.ModuleList(modules) 
+
+    def forward(self, *args, **kwargs):
+        output = self.sub_modules[0](*args, **kwargs)
+        for module in self.sub_modules[1:]:
+            prev_output = output
+            match prev_output, signature(module.forward).parameters.items():
+                case x, [(_, p),] if p.kind in [Parameter.POSITIONAL_ONLY, Parameter.POSITIONAL_OR_KEYWORD]:
+                    #only one parameter, and it's positional
+                    output = module(x)
+                case {**kwargs}, _:
+                    # the dict is interpreted as kwargs
+                    output = module(**kwargs)
+                case t, _ if hasattr(t, "_asdict"):
+                    # the namedtuple is interpreted as kwargs
+                    output = module(**prev_output._asdict())
+                case (tuple(args), {**kwargs}), _:
+                    output = module(*args, **kwargs)
+                case tuple(args), _  :
+                    output = module(*args)
+                case x, _:
+                    output = module(x)
+        return output
