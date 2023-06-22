e55b687fb92bf817dae3399cc0d0bb173757524e
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2023-06-21 20:20:20 -0700

    dataset splitting

--- ml_lib/datasets/__init__.py
+++ ml_lib/datasets/__init__.py
@@ -12,6 +12,13 @@ from . import transforms
 from . import simple_shapes
 from ..register import Loader
 from .registration import register, transform_register
+from .splitting import split_arrays, split_indices
 
 load_dataset = Loader(register)
 """Dataset loader (used by the automated pipeline)"""
+
+__all__ = ['load_dataset', 
+           'register', 
+           'transform_register', 
+           'split_arrays', 
+           'split_indices']
