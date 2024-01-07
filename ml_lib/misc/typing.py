85b9e810b806fc580eb891108fb3bc5be94fdfc1
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-01-07 19:33:53 +0100

    take_annotation_from function small update

--- ml_lib/misc/typing.py
+++ ml_lib/misc/typing.py
@@ -4,9 +4,9 @@ T = TypeVar('T')
 P = ParamSpec('P')
 
 
-def take_annotation_from(this: Callable[P, Optional[T]]) -> Callable[[Callable], Callable[P, Optional[T]]]:
+def take_annotation_from(this: Callable[P, T]) -> Callable[[Callable], Callable[P, T]]:
     """Inspired from https://stackoverflow.com/a/71262408/4948719"""
-    def decorator(real_function: Callable) -> Callable[P, Optional[T]]:
+    def decorator(real_function: Callable) -> Callable[P, T]:
         real_function.__annotations__ = this.__annotations__
         return real_function #type: ignore 
     return decorator
