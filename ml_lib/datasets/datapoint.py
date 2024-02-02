141b288b3ff36ef53b9e01a368ca0ed89b326ed0
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-02-02 13:51:52 +0100

    datapoint

--- ml_lib/datasets/datapoint.py
+++ ml_lib/datasets/datapoint.py
@@ -7,8 +7,8 @@ class Datapoint():
     def get_feature(self, name):
         return getattr(self, name)
 
-    def asdict(self):
-        raise NotImplementedError
+    def asdict(self):                                                              
+        return {f.name: getattr(self, f.name) for f in dataclasses.fields(self)}
 
     def to(self, device, **kwargs):
         return self.__class__(**{name: value.to(device, **kwargs) 
