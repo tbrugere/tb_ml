fd7a5693b9ad0b723833a815e1d99845af475df6
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-03-04 09:04:23 -0800

    added shortcut to multiinputlinear

--- ml_lib/models/layers/__init__.py
+++ ml_lib/models/layers/__init__.py
@@ -1,12 +1,13 @@
 
 from .combinators import ResidualShortcut, Repeat
 from .attention import TransformerBlock
-from .basic import MLP
+from .basic import MLP, MultiInputLinear
 
 __all__ = [
     "ResidualShortcut",
     "Repeat",
     "TransformerBlock",
     "MLP",
+    "MultiInputLinear", 
 ]
 
