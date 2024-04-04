0d8695948944830e080542233a1c465378691d01
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-04-04 11:15:51 -0700

    added debugging and logging contexts

---
+++ ml_lib/misc/context_managers.py
@@ -0,0 +1,37 @@
+import contextlib
+import os
+import logging
+from ml_lib.misc.data_structures import NotSpecified
+from ml_lib.misc.basic import fill_default
+
+
+@contextlib.contextmanager
+def set_num_threads(num_threads: int):
+    import torch
+    old_n_threads = torch.get_num_threads()
+    torch.set_num_threads(num_threads)
+    try:
+        yield
+    finally:
+        torch.set_num_threads(old_n_threads)
+    
+@contextlib.contextmanager
+def set_log_level(level: int, logger: logging.Logger = logging.root):
+    old_level = logger.level
+    logger.setLevel(level)
+    try:
+        yield
+    finally:
+        logger.setLevel(old_level)
+        
+@contextlib.contextmanager
+def torch_profile_auto_save(profiler, trace_file="trace.json"):
+    try:
+        with profiler as prof:
+            yield
+    finally:
+        if "prof" in vars():
+            prof.export_chrome_trace(trace_file)#type:ignore
+        else:
+            logging.warning("Could not export stack trace, profiler didn't get constructed correctly, or got destroyed")
+
--- ml_lib/pipeline/control.py
+++ ml_lib/pipeline/control.py
@@ -1,9 +1,14 @@
 """ml_lib.pipeline.control: Command line and eventually daemon to run experiments"""
-from typing import TYPE_CHECKING, Self, TypeAlias, Literal
-from os import PathLike
+import logging
+from typing import TYPE_CHECKING, ContextManager, Self, TypeAlias, Literal
+import contextlib
 import functools as ft
+import os
+from os import PathLike
 from pathlib import Path
 from logging import getLogger; log = getLogger("__main__")
+
+from ml_lib.misc.context_managers import set_num_threads, set_log_level, torch_profile_auto_save
 
 if TYPE_CHECKING:
     import torch
@@ -40,10 +45,16 @@ class CommandLine():
     resume: str
     only_model: str|int|None = None
 
+    # debugging and utilities
+    log_level: int
+    profile: bool = False
+
     def __init__(self, experiment: PathLike, commands, *,
                  device: "str|torch.device|None"=None, 
                  database: PathLike, resume:str, 
-                 only_model: str|int|None = None): 
+                 only_model: str|int|None = None, 
+                 log_level: int = logging.WARNING, 
+                 profile: bool= False): 
         import torch
         self.experiment_config=Path(experiment)
         if device is None:
@@ -57,13 +68,16 @@ class CommandLine():
             only_model = only_model_
         except ValueError: pass
         self.only_model = only_model
+        self.log_level = log_level
+        self.profile = profile
         
     def run(self):
         with self.database_session() as db_session:
             exp = Experiment.from_yaml(self.experiment_config, 
                                        database_session=db_session)
-            for command in self.commands:
-                self.run_command(exp, command)#type: ignore
+            with self.running_context(): # sets everything up nicely
+                for command in self.commands:
+                    self.run_command(exp, command)#type: ignore
 
     def run_command(self, exp: Experiment, command: CommandType):
         match command:
@@ -96,17 +110,19 @@ class CommandLine():
                    device=args.device, 
                    database=args.database, 
                    resume=args.resume, 
-                   only_model = args.only_model
+                   only_model = args.only_model, 
+                   log_level= args.loglevel, 
+                   profile= args.profile
                    )
 
     
     @staticmethod
     def argument_parser():
-        from argparse import ArgumentParser
+        import argparse
         from pathlib import Path
         import os
 
-        parser = ArgumentParser()
+        parser = argparse.ArgumentParser()
 
         parser.add_argument("config", 
                             type=Path, )
@@ -116,6 +132,24 @@ class CommandLine():
                             default=os.environ.get("EXPERIMENT_DATABASE", "experiment_database.db"))
         parser.add_argument("--resume", type=str, default="highest_step")
         parser.add_argument("--only-model", type=str, default=None)
+        parser.add_argument(
+            '-d', '--debug',
+            help="Print lots of debugging statements",
+            action="store_const", dest="loglevel", const=logging.DEBUG,
+            default=logging.WARNING,
+        )
+        parser.add_argument(
+            '-v', '--verbose',
+            help="Be verbose",
+            action="store_const", dest="loglevel", const=logging.INFO,
+        )
+
+        parser.add_argument(
+            "--profile",
+            help="use profiler", 
+            action=argparse.BooleanOptionalAction,
+            default=False,
+        )
         return parser
 
 
@@ -131,6 +165,40 @@ class CommandLine():
             session.commit()
             session.execute(text("VACUUM"))
 
+
+    @contextlib.contextmanager
+    def running_context(self):
+        """Activates some context managers, and sets some defaults.
+        This is what takes care of 
+        - logging
+        - profiling
+        - setting the number of torch threads
+        - etc."""
+        managers: list[ContextManager] = []
+
+        # maximum number of CPU threads
+        n_threads = os.environ.get("NUM_THREADS")
+        if n_threads is not None:
+            managers.append(set_num_threads(int(n_threads)))
+
+        # nice handling of logging with tqdm
+        try: from tqdm.contrib.logging import logging_redirect_tqdm
+        except ImportError: pass
+        else: managers.append(logging_redirect_tqdm())
+
+        # set logging level
+        managers.append(set_log_level(self.log_level))
+
+        #eventually setup profiling
+        if self.profile:
+            from torch.profiler import profile, record_function, ProfilerActivity
+            profiler = profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True)
+            managers.append(torch_profile_auto_save(profiler, "trace.json"))
+
+        with contextlib.ExitStack() as stack:
+            for m in managers:
+                stack.enter_context(m)
+            yield
 
 
 
