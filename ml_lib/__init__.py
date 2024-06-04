f816f68991a99625f062b92161b2a8fdcb2af893
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-06-04 16:51:37 -0700

    updated configs

--- ml_lib/__init__.py
+++ ml_lib/__init__.py
@@ -1,5 +1,5 @@
 
-from .misc.module_link import lazy_module_link
+# from .misc.module_link import lazy_module_link
 
 # feature_specification = lazy_module_link(f"{__name__}.feature_specification", f"{__name__}.datasets.feature_specification")
 
