871ee8760ad13f622ec53afe5de7223c1803bb2a
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-10-21 15:06:16 -0700

    added positional embeddings

---
+++ ml_lib/models/layers/positional_embeddings.py
@@ -0,0 +1,65 @@
+import warnings
+
+import math
+import torch
+from torch import is_floating_point, nn, Tensor
+
+from ml_lib.misc.torch_functions import ShouldBe as should_be
+
+class PositionalEmbeddings(nn.Module):
+    half_dim: int
+    max_period: int
+    integer_len: int|None
+
+    cache: Tensor|None
+    angular_frequencies: Tensor
+    pi_over_two: Tensor
+
+    def __init__(self, dim, max_period=10000, *,  integer_len=None):
+        assert dim % 2 == 0, 'dim must be even'
+        self.half_dim = dim // 2
+        self.integer_len = integer_len
+        self.max_period = max_period
+        self.cache = None
+        self._init_frequencies()
+        if integer_len is not None:
+            self.cache = self._compute_cache(integer_len)
+            self.register_buffer('cache', self.cache)
+
+
+    @torch.no_grad
+    def _init_frequencies(self):
+        half_dim = self.half_dim
+        exponent = -math.log(self.max_period) * torch.arange(
+            start=0, end=half_dim, dtype=torch.float32, 
+        )
+        exponent = exponent / (half_dim)
+        self.angular_frequencies= torch.exp(exponent)
+
+    @torch.no_grad
+    def _compute_cache(self, integer_len):
+        return self.compute_embeddings(torch.linspace(0, 1, integer_len))
+
+    def compute_embeddings(self, fractional_timesteps):
+        """fractional timesteps are between 0 and 1"""
+        *batch, = fractional_timesteps.shape
+
+        phases = self.angular_frequencies * fractional_timesteps   <= should_be(*batch, self.half_dim)
+        phases = phases[..., None].broadcast_to(*batch, self.half_dim, 2)
+        phases[..., 1] += self.pi_over_two
+
+        phases = phases.reshape(*batch, self.half_dim * 2).contiguous()
+        phases = torch.sin(phases)
+        return phases
+
+
+    def forward(self, timesteps):
+        if torch.is_grad_enabled():
+            warnings.warn("forgot to disable grad when computing embeddings")
+
+        if torch.is_floating_point(timesteps):
+            return self.compute_embeddings(timesteps)
+        else:
+            assert self.cache is not None
+            return self.cache[timesteps]
+
