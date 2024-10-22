ca49f3e99568bd3906116403d731da4478e7f088
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-10-22 14:17:13 -0700

    updates

--- ml_lib/misc/matchers.py
+++ ml_lib/misc/matchers.py
@@ -1,6 +1,6 @@
 """ABCs used to make match-case statements more readable
 A lot of this is very dark magic, 
-basically subclasses of Checker hijack insinsatnce calls:
+basically subclasses of Checker hijack insinsantce calls:
 they all define a check() classmethod, 
 and A.check(x) is called when trying to check if isinstance(x, A)"""
 from typing import Callable, Any
@@ -8,19 +8,21 @@ from typing import Callable, Any
 class CheckerMeta(type):
     check: Callable[[Any], bool]
 
+    def __call__(cls, *args, **kwargs):
+        del args, kwargs
+        raise ValueError("Class with metaclass CheckerMeta shouldn't be instantiated")
+
     def __instancecheck__(self, instance):
         return self.check(instance)
 
 class Checker(metaclass=CheckerMeta):
-    def __init__(self):
-        raise ValueError("Class with metaclass CheckerMeta shouldn't be instantiated")
-
     @classmethod
     def check(cls, instance) -> bool:
         del instance
         return False
 
 class EmptySet(Checker):
+    """``isinstance(x, EmptySet)`` checks if x is an empty ``set()`` instance."""
     @classmethod
     def check(cls, instance):
         return isinstance(instance, set) and len(instance) == 0
