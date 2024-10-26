6e5c9b60105080753da09b1b2aa1db626b4310b4
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-10-26 16:28:22 -0700

    added some documentation

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # ml_lib
 
-Misc tools, models, and data structures I use for machine learning with pytorch. 
+Tools for fast prototyping of machine learning models in pytorch.
+
+This library is a collection of tools I have built over time while prototyping machine learning models in pytorch. 
+This is not focused on production, and is intedend for my use-case, which is developing new models, with a focus on experimentation. 
+In particular it has no support for multi-gpu or distributed training, or sharing model weights.
+For a more complete tool, I recommend looking at [pytorch-lightning](https://lightning.ai/docs/pytorch/stable/)
+
+This is a work in progress and relatively unstable, 
+
+## Features
+
+- [x] Systematic definition of model hyperparameters from type definitions [see the Model class](ml_lib/models)
+- [x] Basically everything from the command line interface to loading every training / model / dataset / etc. parameters from configs to the training code. Only model and dataset definitions are left to the user.
+- [x] `Trainer` class for training models from configuration files, with automatic checkpointing, logging to database, and more…
+- [x] Saving and loading models / parameters / experiments to a sqlite database
+- [x] A lot of miscelanneous features such as printing model sizes as trees, keeping track of a model's device, freezing models, checking all given input to a model are on the right device, automatically creating tensors on the right device, etc.
+- [ ] Job control for sending jobs to several machines / vms / cloud workers
+
+
--- ml_lib/models/__init__.py
+++ ml_lib/models/__init__.py
@@ -1,3 +1,51 @@
+"""
+This module contains the base classes for models, as well as some layer implementations for neural networks.
+
+The most important class here the :class:`Model` class. Every model should inherit from this class. (but not layers which are classic :class:`torch.nn.Module`)
+
+Base usage:
+
+.. code-block:: python
+   from typing import Literal
+   from ml_lib.models import Model, Hyperparameter
+   from ml_lib.models.layers import MLP
+   class MyModel(ml_lib.models.Model):
+       n_layers: Hyperparameter[int]
+       input_dim: Hyperparameter[int]
+       output_dim: Hyperparameter[int]
+       hidden_dim: Hyperparameter[int] = 512
+       loss: Hyperparameter[Literal["mse", "cross_entropy"]] = "mse
+
+       inner: MLP
+       loss_function: torch.nn.MSELoss
+
+       def __setup__(self):
+           # construct the model from hyperparameter values
+           # in this function, the hyperparameter values are already populated
+           self.inner = MLP(input_dim, *[hidden_dim]*n_layers - 1, output_dim)
+
+       def compute_loss(self, x, y):
+           z = self.inner(x)
+           return self.loss_function(z, y)
+
+    model = MyModel(n_layers=3, input_dim=10, output_dim=1)
+
+Notice the absence of a constructor: one should already be provided by :class:`Model`, that inputs the hyperparameter as keyword-only arguments, calls ``nn.Module.__init__`` 
+**checks types for all the hyperparameters**
+
+Using this class will have several benefits out of the box, apart from not forgetting to call ``super().__init__``, and it working out of the box with :class:`ml_lib.pipeline.Trainer`: 
+
+The model has a well-defined device, accessible with ``model.device`` (which changes when the model is moved to a different device.)
+All methods of the module are automatically run as if they were in the context manager
+.. code-block:: python
+   with self.device:
+       ...
+which means all tensors are created on the correct device by default.
+The arguments to functions will also be checked to see if they are on model device.
+
+(all these behaviours can be deactivated)
+"""
+
 from .base_classes import Model, Supervised, Unsupervised, Hyperparameter
 
 from ..register import Loader
