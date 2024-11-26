f3b1c73592c3b33996e56a5ae0cc03a0d2ed0513
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2024-11-25 22:50:47 -0800

    indentation error?

--- ml_lib/models/diffusion/training_helpers.py
+++ ml_lib/models/diffusion/training_helpers.py
@@ -43,10 +43,10 @@ class DiffusionTrainingNoiser():
         x_t = a_t * x_1 + b_t * noise
         """
         raise NotImplementedError
-        
+
 
 class RectifiedFlowNoise(DiffusionTrainingNoiser):
-   def apply_noise(self, x, noise, timesteps):
+    def apply_noise(self, x, noise, timesteps):
         return (1 - timesteps[:, None]) * x + timesteps[:, None] * noise
 
     def epsilon_to_v(self, x, epsilon):
@@ -89,13 +89,13 @@ class DiffusionTrainingHelperMixin():
     _n_timesteps: int|None = None
     _noiser: DiffusionTrainingNoiser
 
-    
+
 
     def __init__(self, *,  prediction_type: PredictionType = "noise", timestep_sampling_method: Timestep_sampling_method = "uniform", timesteps: int|None = None, noise_type: Literal["cosine", "exploding", "rectified_flow"]):
         self._diff_train_prediction_type = prediction_type
         self._diff_train_timestep_sampling_method = timestep_sampling_method
         if not self._integer_timesteps and timesteps is not None:
-                raise ValueError("timesteps should be None if the model does not use integer timesteps (assumed to be [0-1])")
+            raise ValueError("timesteps should be None if the model does not use integer timesteps (assumed to be [0-1])")
         elif self._integer_timesteps and timesteps is None: 
             raise ValueError("timesteps should be provided if the model uses integer timesteps")
         self._n_timesteps = timesteps
@@ -152,7 +152,7 @@ class DiffusionTrainingHelperMixin():
             case _:
                 raise ValueError(f"Cannot convert from {convert_from} to {convert_to}")
 
-    
+
     @torch.no_grad()
     def apply_noise(self, x, noise, timesteps):
         """apply noise to the input point"""
