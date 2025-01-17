59abc14654374d40a1fc6b3779ec33504f86097e
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2025-01-17 14:38:44 -0800

    repair eror with MultiEpochDataLoader

--- ml_lib/datasets/utils.py
+++ ml_lib/datasets/utils.py
@@ -1,17 +1,33 @@
-"""DataLoader that preloads data for the next epoch
-
-useful for traning when data loiding is intensive and takes time
-(avoids slowdowns between epochs)
-
-https://github.com/pytorch/pytorch/issues/15849#issuecomment-518126031
-"""
 import torch
 import torch.utils.data
 
 
+class MultiEpochDataLoader(torch.utils.data.DataLoader):
+    """DataLoader that preloads data for the next epoch
+
+    useful for traning when data loiding is intensive and takes time
+    (avoids slowdowns between epochs)
+
+    https://github.com/pytorch/pytorch/issues/15849#issuecomment-518126031
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._DataLoader__initialized = False
+        self.batch_sampler = _RepeatSampler(self.batch_sampler)
+        self._DataLoader__initialized = True
+        self.iterator = super().__iter__()
+
+    def __len__(self):
+        return len(self.batch_sampler.sampler)
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield next(self.iterator)
+
+
 class _RepeatSampler(object):
     """ Sampler that repeats forever.
-
     Args:
         sampler (Sampler)
     """
@@ -22,19 +38,3 @@ class _RepeatSampler(object):
     def __iter__(self):
         while True:
             yield from iter(self.sampler)
-
-
-class MultiEpochDataLoader(torch.utils.data.dataloader.DataLoader):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.batch_sampler = _RepeatSampler(self.batch_sampler)
-        self.iterator = super().__iter__()
-
-    def __len__(self):
-        return len(self.batch_sampler.sampler)
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield next(self.iterator)
-
